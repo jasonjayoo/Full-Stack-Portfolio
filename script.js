c371f56a7f6e3763ecaef99000a603aa20ecc15a
jasonjayoo <94942855+jasonjayoo@users.noreply.github.com>
2022-04-09 17:07:27 -0700

    vars for hamburger nav

---
+++ script.js
@@ -0,0 +1,6 @@
+// variables for hamburger nav bar
+
+let hamburger = document.querySelector('.header .nav-bar .nav-list .hamburger');
+let mobile_menu = document.querySelector('.header .nav-bar .nav-list ul');
+let menu_item = document.querySelectorAll('.header .nav-bar .nav-list ul li a');
+let header = document.querySelector('.header.container');
