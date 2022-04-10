05b2b7044230e77e2ce676fe5d79a4c74ea84f0b
jasonjayoo <94942855+jasonjayoo@users.noreply.github.com>
2022-04-09 17:17:45 -0700

    added rest of styles and update script.js file

--- script.js
+++ script.js
@@ -1,33 +1,32 @@
 // variables for hamburger nav bar
 
-let hamburger = document.querySelector('.header .nav-bar .nav-list .hamburger');
-let mobile_menu = document.querySelector('.header .nav-bar .nav-list ul');
-let menu_item = document.querySelectorAll('.header .nav-bar .nav-list ul li a');
-let header = document.querySelector('.header.container');
+let hamburger = document.querySelector(".header .nav-bar .nav-list .hamburger");
+let mobile_menu = document.querySelector(".header .nav-bar .nav-list ul");
+let menu_item = document.querySelectorAll(".header .nav-bar .nav-list ul li a");
+let header = document.querySelector(".header.container");
 
 // nav bar implementation
-hamburger.addEventListener('click', () => {
-	hamburger.classList.toggle('active');
-	mobile_menu.classList.toggle('active');
+hamburger.addEventListener("click", () => {
+  hamburger.classList.toggle("active");
+  mobile_menu.classList.toggle("active");
 });
 
 // nav bar menu item event listeners
 menu_item.forEach((item) => {
-	item.addEventListener('click', () => {
-		hamburger.classList.toggle('active');
-		mobile_menu.classList.toggle('active');
-	});
+  item.addEventListener("click", () => {
+    hamburger.classList.toggle("active");
+    mobile_menu.classList.toggle("active");
+  });
 });
 
-
 // moment js for displaying date and time set to 5 second interval of updating
-$('#date').text(moment().format('LLLL'));
+$("#date").text(moment().format("LLLL"));
 
-function refresh(){
-	var time = setInterval(function () {
-		$('#date').text(moment().format('LLLL'));
-	},5000); 
-	time;
-};
+function refresh() {
+  var time = setInterval(function () {
+    $("#date").text(moment().format("LLLL"));
+  }, 5000);
+  time;
+}
 
-refresh()
+refresh();
