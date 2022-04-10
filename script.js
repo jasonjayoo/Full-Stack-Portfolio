938fa40d86a2e077fd91d6017ac1b9e22e460812
jasonjayoo <94942855+jasonjayoo@users.noreply.github.com>
2022-04-09 17:08:03 -0700

    hamburger nav bar event listener added

--- script.js
+++ script.js
@@ -4,3 +4,10 @@ let hamburger = document.querySelector('.header .nav-bar .nav-list .hamburger');
 let mobile_menu = document.querySelector('.header .nav-bar .nav-list ul');
 let menu_item = document.querySelectorAll('.header .nav-bar .nav-list ul li a');
 let header = document.querySelector('.header.container');
+
+// nav bar implementation
+hamburger.addEventListener('click', () => {
+	hamburger.classList.toggle('active');
+	mobile_menu.classList.toggle('active');
+});
+
